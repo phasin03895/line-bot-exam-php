72d96e48137b42a7b25ca49e50429eb26033b7e8
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-05-26 16:45:32 +0700

    test four

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'fVTYgKqjwvwQtFKWhA2t+M5HYNe2Qw5n4zN5tuM9Vtf+bNI2r69cL1clRAwok4p5eZdTZBfLFMsPBEzzu/1e/lQItqc9cvb305G47YMuyzdg+MxWUii2Gj16JOlUiJRiImZcHPXLJwvuPGHHu/Uq8QdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '62e3700fa8bc5f551326d04befce24eb';
+$channelSecret = '6c2096554f1cfd23d4e61effa45c5b75';
 
 $pushID = 'Uf3d584c146e3db13809b30d35665e105';
 
