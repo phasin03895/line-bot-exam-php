0532a0538e3bde5864a37477cb976dce54cc9b7a
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-05-26 16:53:42 +0700

    xxx

--- webhooks.php
+++ webhooks.php
@@ -45,28 +45,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-		}
-		// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
+			
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
@@ -81,6 +60,7 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
+		
 	}
 }
 echo "OK";
