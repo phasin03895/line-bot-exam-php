6096884405260110c1ae0727e612250979313baf
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-05-30 22:40:48 +0700

    six

--- botpush.php
+++ botpush.php
@@ -2,19 +2,11 @@
 
 
 
-require "vendor/autoload.php";
-
-$access_token = 'fVTYgKqjwvwQtFKWhA2t+M5HYNe2Qw5n4zN5tuM9Vtf+bNI2r69cL1clRAwok4p5eZdTZBfLFMsPBEzzu/1e/lQItqc9cvb305G47YMuyzdg+MxWUii2Gj16JOlUiJRiImZcHPXLJwvuPGHHu/Uq8QdB04t89/1O/w1cDnyilFU=';
-
-$channelSecret = '6c2096554f1cfd23d4e61effa45c5b75';
-
-$pushID = 'Uf3d584c146e3db13809b30d35665e105';
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('fVTYgKqjwvwQtFKWhA2t+M5HYNe2Qw5n4zN5tuM9Vtf+bNI2r69cL1clRAwok4p5eZdTZBfLFMsPBEzzu/1e/lQItqc9cvb305G47YMuyzdg+MxWUii2Gj16JOlUiJRiImZcHPXLJwvuPGHHu/Uq8QdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '6c2096554f1cfd23d4e61effa45c5b75']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
+$response = $bot->pushMessage('Uf3d584c146e3db13809b30d35665e105', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
