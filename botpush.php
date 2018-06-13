06751ca3498a957a116889d465e37d3704055042
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-06-13 15:17:04 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,10 +1,11 @@
 <?php
+echo "- - - - - - - - - - -  - - -  - - - - - - - - - 2";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('fVTYgKqjwvwQtFKWhA2t+M5HYNe2Qw5n4zN5tuM9Vtf+bNI2r69cL1clRAwok4p5eZdTZBfLFMsPBEzzu/1e/lQItqc9cvb305G47YMuyzdg+MxWUii2Gj16JOlUiJRiImZcHPXLJwvuPGHHu/Uq8QdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'b3a68d22e90c3bbb52469919115429d0']);
-
+echo "- - - - - - - - - - -  - - -  - - - - - - - - - -2";
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 $response = $bot->pushMessage('Uf3d584c146e3db13809b30d35665e105', $textMessageBuilder);
-
+echo "- - - - - - - - - - -  - - -  - - - - - - - - - 3";
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 echo "- - - - - - - - - - -  - - -  - - - - - - - - - -";
 ?>
