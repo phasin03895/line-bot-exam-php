23c2563e1842973711e3b7500a4fbb76978cf9d9
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-06-15 11:39:40 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			$text = $text+"\nPahsin Aumwong";
+			$text .="\nPahsin Aumwong";
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
