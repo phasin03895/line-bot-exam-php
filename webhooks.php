ab0583c13e6525eb1523e284f8658d40360249c1
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-07-06 11:42:34 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -33,7 +33,7 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' );
 
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
