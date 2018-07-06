7aeb037c711d81f58c2a7c71b5c1f43424f847f3
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-07-06 11:18:23 +0700

    Update testpushbot.php

--- testpushbot.php
+++ testpushbot.php
@@ -1,3 +1,4 @@
+<?php
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->replyMessage('Uf3d584c146e3db13809b30d35665e105', $textMessageBuilder);
 if ($response->isSucceeded()) {
