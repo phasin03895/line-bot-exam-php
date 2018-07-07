179852a4715ce2687609e81eee432c0fa5aeb17c
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-07-07 22:01:36 +0700

    Update ahha01.php

--- ahha01.php
+++ ahha01.php
@@ -16,12 +16,13 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
-      $non=$event['message']['text'];
+      $non[100]=$event['message']['text'];
       $ni=0;
       $nj=0;
       $nk=0;
       $nl=0;
       $nchk=0;
+	$nonb[100]={};
 
       $nl=strlen($non);
         for($ni;$ni<$nl;$ni++)
