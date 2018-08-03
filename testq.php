023aa77882dec43d9d077ee13c9d1ff4fe20d9bf
phasin03895 <34596249+phasin03895@users.noreply.github.com>
2018-08-03 10:06:20 +0700

    Update testq.php

--- testq.php
+++ testq.php
@@ -28,11 +28,11 @@
       }
       $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
       $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = $mess;
+      $arrayPostData['messages'][0]['text'] = "$mess";
         $arrayPostData['messages'][1]['type'] = "text";
-      $arrayPostData['messages'][1]['text'] = $stringl;
+      $arrayPostData['messages'][1]['text'] = "$stringl";
         $arrayPostData['messages'][2]['type'] = "text";
-      $arrayPostData['messages'][2]['text'] = $count;
+      $arrayPostData['messages'][2]['text'] ="$count";
       replyMsg($arrayHeader,$arrayPostData);
         /*
       if($mess == "ของไหล"){
